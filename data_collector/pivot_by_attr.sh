4bd7cf2528015b227c5368e90cbb0a148a8b950d
yasny <yasny.jp@gmail.com>
2014-11-15 17:38:00 +0900

    sqliteからCSVデータを取得するようにした。

--- data_collector/pivot_by_attr.sh
+++ data_collector/pivot_by_attr.sh
@@ -3,19 +3,26 @@
 BASE_DIR=$( dirname $0 )
 TDL_DIR="$BASE_DIR/tdl"
 TDS_DIR="$BASE_DIR/tds"
+DB="$BASE_DIR/disney.sqlite"
 
 OUTPUT_CSV="$BASE_DIR/all.csv"
 
+function execute_sql_csv {
+  echo $1 | sqlite3 -csv $DB
+}
+
+function get_data_for_attr {
+  execute_sql_csv "select d.datetime,a.name,d.wait from data as d join attractions as a on d.attraction_id=a.id where a.name=\"$1\" and datetime > date(\"now\",\"-7 days\") order by d.datetime;"
+}
+
 echo "date,attr,wait" > $OUTPUT_CSV
-grep -h "ジャングル" $TDL_DIR/*.dat | cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "スペース" $TDL_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "スプラッシュ" $TDL_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "ツアーズ" $TDL_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "ホーンテッド" $TDL_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "プーさん" $TDL_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
+get_data_for_attr "ジャングルクルーズ：ワイルドライフ・エクスペディション" >> $OUTPUT_CSV
+get_data_for_attr "スペース・マウンテン" >> $OUTPUT_CSV
+get_data_for_attr "スプラッシュ・マウンテン" >> $OUTPUT_CSV
+get_data_for_attr "スター・ツアーズ：ザ・アドベンチャーズ・コンティニュー" >> $OUTPUT_CSV
+get_data_for_attr "ホーンテッドマンション" >> $OUTPUT_CSV
+get_data_for_attr "プーさんのハニーハント" >> $OUTPUT_CSV
 
-grep -h "インディ" $TDS_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "タワー" $TDS_DIR/*.dat |cut -d"," -f1,2,5 >> $OUTPUT_CSV
-grep -h "レイジング" $TDS_DIR/*.dat | cut -d"," -f1,2,5 >> $OUTPUT_CSV
-
+get_data_for_attr "インディ・ジョーンズ（R）・アドベンチャー：クリスタルスカルの魔宮" >> $OUTPUT_CSV
+get_data_for_attr "タワー・オブ・テラー" >> $OUTPUT_CSV
 
